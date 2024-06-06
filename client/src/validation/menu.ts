ab29c393c555bbe831acbc60e54d896ec4db65b4
Heisey <jheisler01@gmail.com>
2024-06-06 15:16:02 -0700

    fixed validation from type change

--- client/src/validation/menu.ts
+++ client/src/validation/menu.ts
@@ -4,7 +4,7 @@ import * as Validator from 'zod'
 import * as Core from '@/core'
 import * as Validate from '.'
 
-export const menu: Validator.z.ZodType<Core.I.Menu> = Validator.z.object({
+export const menu: Validator.z.ZodType<Core.I.MenuInfo> = Validator.z.object({
   // id: Validator.z.string(),
   appetizers: Validator.z.array(Validate.menuItem),
   mains: Validator.z.array(Validate.menuItem).optional(),
--- client/src/validation/menuItem.ts
+++ client/src/validation/menuItem.ts
@@ -3,12 +3,16 @@ import * as Validator from 'zod'
 
 import * as Core from '@/core'
 
-export const menuItem: Validator.z.ZodType<Core.I.MenuItem> = Validator.z.object({
-  // id: Validator.z.string(),
+export const menuItem: Validator.z.ZodType<Core.I.MenuItemRecord> = Validator.z.object({
+  id: Validator.z.string(),
   name: Validator.z.string().min(1, 'Menu item must have a name'),
   price: Validator.z.coerce.number().min(0.99, 'Price item must have a min price of 0.99').transform(data => Number(data)),
-  // ingredients: Validator.z.array(Validator.z.string()).min(1, 'Must list atleast 1 ingredient'),
-  // description: Validator.z.string().min(1, 'must provide a description')
+  ingredients: Validator.z.array(Validator.z.string()).min(1, 'Must list atleast 1 ingredient'),
+  description: Validator.z.string().min(1, 'must provide a description'),
+  resturantId: Validator.z.string().min(1, 'must provide a resturant id'),
+  menuId: Validator.z.string(),
+  createdAt: Validator.z.date(),
+  updatedAt: Validator.z.date()
 })
 
 export type MenuItemValidate = Validator.z.infer<typeof menuItem>
