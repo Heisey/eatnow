ec18512562f9837225013a248d276b3acfeb06cf
Heisey <jheisler01@gmail.com>
2024-06-15 00:05:25 -0700

    changed auth providers in server

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,7 @@ require('dotenv').config();
 import chalk from 'chalk'
 import cors from 'cors'
 import express from 'express'
+import * as firebase from 'firebase-admin/app'
 import helmut from 'helmet'
 import morgan from 'morgan'
 
@@ -24,7 +25,7 @@ app.use('/api/v1/resturants', router.resturants)
 app.use('/api/v1/user', router.user)
 
 const start = async () => {
-
+  firebase.initializeApp()
   app.listen(PORT, () => console.log(`${chalk.blue.bold('Server started on port')} ${chalk.yellowBright.bold(PORT)}`))
   await db.connection()
 }
--- server/src/middleware/authentication.ts
+++ server/src/middleware/authentication.ts
@@ -1,9 +1,19 @@
 
-import * as Bearer from 'express-oauth2-jwt-bearer'
+import express from 'express'
+import firebase from 'firebase-admin/app-check'
 
-export const jwtCheck = Bearer.auth({
-  audience: process.env.AUTH_AUDIENCE,
-  issuerBaseURL: process.env.AUTH_ISSUER_BASE_URL,
-  tokenSigningAlg: process.env.AUTH_TOKEN_SIGNING_ALG
-})
+export const jwtCheck = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  try {
+    const token = req.header('Authorization')?.split(' ')[1]
 
+    if (!token) return res.status(401).json({ message: 'Unauthorized' })
+
+    const verified = await firebase.getAppCheck().verifyToken(token)
+
+    if (!verified) return res.status(401).json({ message: 'Unauthorized'})
+
+    next()
+  } catch(err) {
+    res.status(401).json({ message: 'Unauthorized'})
+  }
+}
