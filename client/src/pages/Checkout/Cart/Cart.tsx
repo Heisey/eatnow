cd1008beeea6007ee7597006b3c4c9cc84b4acae
Heisey <jheisler01@gmail.com>
2024-06-27 16:14:52 -0700

    finished order page

--- client/src/pages/Checkout/Cart/Cart.tsx
+++ client/src/pages/Checkout/Cart/Cart.tsx
@@ -26,6 +26,7 @@ const Cart: React.FC<CartProps> = (props) => {
     columnConfig.accessor('price', { header: 'Price', cell: dataSet => `$${dataSet.getValue()}`}),
     columnConfig.accessor('price', { header: () => <div className='text-right'>'Item Total'</div>, cell: dataSet => <div className='text-right'>`$${dataSet.row.getUniqueValues('quantity')[0] as number * dataSet.getValue()}`</div> })
   ]
+
   const table = ReactTable.useReactTable({
     data: appCtx.cart.items,
     columns: columns,
@@ -38,11 +39,13 @@ const Cart: React.FC<CartProps> = (props) => {
     return result
   }
 
+  const calcTax = calcSubTotal() * 0.05
+
   return (
-    <div>
-      <h2>Order</h2>
+    <div className='max-w-[900px] mx-auto'>
+      <h2 className='mb-8 font-bold text-2xl text-orange-500'>Order</h2>
       <div>
-        <Table className='mb-4 max-w-[900px] mx-auto'>
+        <Table className='mb-4 border-b-2 border-orange-500'>
           <TableHeader>
             {table.getHeaderGroups().map(row => (
               <TableRow key={row.id}>
@@ -66,9 +69,23 @@ const Cart: React.FC<CartProps> = (props) => {
             ))}
           </TableBody>
         </Table>
-        <div className='max-w-[900px] mx-auto flex items-center justify-end'>
-          <span className='mr-2 font-bold'>Sub Total</span>
-          <span>{`$${calcSubTotal()}`}</span>
+        <div className='flex flex-col items-end'>
+          <div className='flex items-center justify-end mb-2'>
+            <span className='mr-2 font-bold'>Sub Total</span>
+            <span>{`$${calcSubTotal()}`}</span>
+          </div>
+          <div className='flex items-center justify-end mb-2'>
+            <span className='mr-2 font-bold'>Tax</span>
+            <span>{`$${calcTax}`}</span>
+          </div>
+          <div className='flex items-center justify-end border-b-2 border-orange-500 mb-2'>
+            <span className='mr-2 font-bold'>Delivery Price</span>
+            <span>{`$${appCtx.cart.deliveryPrice}`}</span>
+          </div>
+          <div className='flex items-center justify-end'>
+            <span className='mr-2 font-bold'>Total</span>
+            <span>{`$${calcSubTotal() + calcTax + appCtx.cart.deliveryPrice}`}</span>
+          </div>
         </div>
       </div>
     </div>
